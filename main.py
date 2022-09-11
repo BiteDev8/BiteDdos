8e6015bf9e76335be4e7dce3caee9dc01c75f348
BiteDev <113308968+BiteDev8@users.noreply.github.com>
2022-09-11 21:27:31 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from colorama import Fore, Back, Style, init
+init()
+print(Fore.GREEN+"Discord tag a doss:")
+x = str(input())
+z =1
+while z <= 15000 :
+    print(z ," ping :", x)
+    z+=1
+while True:
+     print("ddos reussi")
